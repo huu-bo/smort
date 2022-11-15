e20f4324ae58ba899b366a9a7398ad5207b78447
huu-bo <huubhoogweg@gmail.com>
2022-11-15 20:29:03 +0100

    useful commit message

---
+++ editor.py
@@ -0,0 +1,185 @@
+import string
+import greekUtils as gu
+
+
+# https://stackoverflow.com/a/21659588
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys
+    import tty
+
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+
+getch = _find_getch()
+
+
+def print_g():
+    global typing
+    print('\r' + ' ' * 80 + '\r', end='')
+    i = 0
+    for c in typing:
+        if type(c) != gu.Letter:
+            if c == 'σ':
+                if i < len(typing) - 1:
+                    if typing[i + 1] not in ' \n':
+                        print(c, end='')
+                    else:
+                        print('ς', end='')
+                else:
+                    print('ς', end='')
+            else:
+                print(c, end='')
+        else:
+            print(c.unicode(), end='')
+        i += 1
+
+
+greek = {
+    'a': 'αΑ',
+    'b': 'βΒ',
+    'g': 'γΓ',
+    'd': 'δΔ',
+    'e': 'εΕ',
+    'z': 'ζΖ',
+    'h': 'ηΗ',
+    'q': 'θΘ',  # the second one is capital here too
+    'i': 'ιΙ',
+    'k': 'κΚ',
+    'l': 'λΛ',
+    'm': 'μΜ',
+    'n': 'νΝ',
+    'c': 'ξΖ',
+    'o': 'οΟ',
+    'p': 'πΠ',
+    'r': 'ρΡ',
+    's': 'σΣ',
+    't': 'τΤ',
+    'u': 'υΥ',
+    'f': 'φΠ',
+    'x': 'χΧ',
+    'y': 'ψΨ',
+    'w': 'ωΩ'
+}
+
+out = []
+lang = 'L'
+typing = []
+c = 'T'
+while True:
+    c = getch()
+
+    if hex(ord(c)) == '0x3':  # ^C
+        break
+    elif hex(ord(c)) == '0xd':  # newline
+        split = [[]]
+        i = 0
+        for c in typing:
+            if c != ' ':
+                split[-1].append(c)
+            else:
+                split.append([])
+                split[-1] += typing[i+1:]
+                break
+            i += 1
+        if len(split) == 2:
+            out_ss = []
+            for s in split:
+                out_s = []
+                for c in s:
+                    if type(c) == gu.Letter:
+                        out_s.append(c.unicode())
+                    else:
+                        out_s.append(c)
+                out_ss.append(''.join(out_s))
+            # if len(out_ss) > 2:
+            #     i = 2
+            #     print(out_ss)
+            #     while i < len(out_ss):
+            #         out_ss[1] = out_ss[1].join(out_ss.pop(i))
+            #         print(out_ss)
+            #         # i += 1
+            out.append(out_ss)
+
+        typing = []
+        print_g()
+        continue
+    elif hex(ord(c)) == '0x7f':  # backspace
+        typing = typing[:-1]
+        print_g()
+        continue
+    elif c == ' ':
+        typing += ' '
+        print_g()
+        continue
+    elif c == 'G':
+        lang = 'G'
+        continue
+    elif c == 'L':
+        lang = 'L'
+        continue
+
+    elif c == '(' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.spiritus = 1
+        print_g()
+        continue
+    elif c == ')' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.spiritus = -1
+        print_g()
+        continue
+    elif c == '/' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.acutus = 1
+        print_g()
+        continue
+    elif c == '\\' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.acutus = -1
+        print_g()
+        continue
+    elif c == '=' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.circumflex = True
+        print_g()
+        continue
+    elif c == '|' and type(typing[-1]) == gu.Letter:
+        typing[-1].mod.iota = True
+        print_g()
+        continue
+
+    # else:
+    #     print(hex(ord(c)))
+
+    if lang == 'G':
+        if c.lower() not in greek:
+            continue
+
+        if c in string.ascii_uppercase:
+            c = gu.Letter(c.lower())
+        else:
+            c = gu.Letter(c)
+
+    typing.append(c)
+    print_g()
+
+print()
+print(str(out).replace("'", '"'))
+
+filename = 'quizzes/ARGO grieks/' + input('filename quizzes/ARGO grieks/')
+
+with open(filename, 'w') as file:
+    file.write(str(out).replace("'", '"'))
---
+++ greekUtils.py
@@ -0,0 +1,137 @@
+import typing
+
+# α ἀ ἁ ὰ ά ᾶ ἂ  ἃ  ἄ  ἅ  ἆ  ἇ
+#   ) ( \ / = )\ (\ )/ (/ )= (=
+# 0 1 2 3 4 5 6  7  8  9  10 11
+
+
+class Lut:
+    def __init__(self, filename):
+        with open(filename, 'r') as file:
+            raw = file.read()
+        self.data = raw.split('\n')
+
+        self.dict = {}
+        for line in self.data:
+            if not line:
+                continue
+            self.dict[line[0]] = line
+
+    def __getitem__(self, item):
+        if type(item[0]) == int:
+            return self.data[item[1]][item[0]]
+        elif type(item[0]) == str:
+            if item[0] in self.dict:
+                return self.dict[item[0]][item[1]]
+
+
+class Mod:
+    def __init__(self):
+        self.spiritus = 0  # -1 lenis/asper 1
+        self.acutus = 0  # -1 acutus/gravis 1
+        self.circumflex = False
+        self.iota = False
+
+    def __setattr__(self, key, value):
+        if key == 'spiritus':
+            if value in [-1, 0, 1]:
+                self.__dict__['spiritus'] = value
+        elif key == 'acutus':
+            if value in [-1, 0, 1]:
+                self.__dict__['acutus'] = value
+                self.__dict__['circumflex'] = False
+        elif key == 'circumflex':
+            self.__dict__['circumflex'] = value
+            self.__dict__['acutus'] = 0
+        elif key == 'iota':
+            self.__dict__['iota'] = value
+        else:
+            raise AttributeError(f'{key} not in {self}')
+
+    def unicode(self):
+        return
+
+
+greek = {
+    'a': 'αΑ', 'b': 'βΒ', 'g': 'γΓ', 'd': 'δΔ', 'e': 'εΕ', 'z': 'ζΖ', 'h': 'ηΗ', 'q': 'θΘ', 'i': 'ιΙ', 'k': 'κΚ',
+    'l': 'λΛ', 'm': 'μΜ', 'n': 'νΝ', 'c': 'ξΖ', 'o': 'οΟ', 'p': 'πΠ', 'r': 'ρΡ', 's': 'σΣ', 't': 'τΤ', 'u': 'υΥ',
+    'f': 'φΠ', 'x': 'χΧ', 'y': 'ψΨ', 'w': 'ωΩ'
+}
+
+
+class Letter:
+    def __init__(self, char, mod=None, filename='G_LUT.txt'):
+        self.char = greek[char][0]
+        if mod is not None:
+            self.mod = mod
+        else:
+            self.mod = Mod()
+
+        self.LUT = Lut(filename)
+
+    def unicode(self):
+        if self.char not in self.LUT.dict:
+            return self.char
+
+        n = 0
+
+        if self.mod.spiritus == -1:
+            if self.mod.acutus == -1:
+                n = 6
+            elif self.mod.acutus == 1:
+                n = 8
+            elif self.mod.circumflex:
+                n = 10
+            else:
+                n = 1
+        elif self.mod.spiritus == 1:
+            if self.mod.acutus == -1:
+                n = 7
+            elif self.mod.acutus == 1:
+                n = 9
+            elif self.mod.circumflex:
+                n = 11
+            else:
+                n = 2
+        else:
+            if self.mod.acutus == -1:
+                n = 3
+            elif self.mod.acutus == 1:
+                n = 4
+            elif self.mod.circumflex:
+                n = 5
+            else:
+                n = 0
+
+        n += 12 * self.mod.iota
+
+        return self.LUT[self.char, n]
+
+
+class GreekStr:
+    def __init__(self, data: str):
+        """
+        creates a string for ancient greek
+        :param data: str of latin characters to be converted to ancient greek characters
+        """
+        self.data = []
+        self + data
+
+    def __repr__(self) -> str:
+        return ''.join([letter.unicode() for letter in self.data])
+
+    def __add__(self, other):
+        if type(other) == GreekStr:
+            self.data += other
+        elif type(other) == str:
+            for c in other:
+                self.data.append(Letter(c))
+        else:
+            raise TypeError('GreekStr')
+
+
+if __name__ == '__main__':
+    s = GreekStr('test')
+    print(s)
+
+    # TODO: add tests
